624b15ab6a05ee28b2205a98a47309e0ce7db838
zanniadevweb <zanni.a.dev.web@gmail.com>
2023-08-04 00:19:24 +0200

    try other deploy

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@
               />
 
               <h3 class="text-h3">
-                Try the new Vuetify
+                Try the new Vuetify !!!
               </h3>
 
               <p class="mt-4 text-medium-emphasis">
