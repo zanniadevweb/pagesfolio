d5c73cf5263bbb80fd735eee85be9c30921a3b31
zanniadevweb <zanni.a.dev.web@gmail.com>
2023-08-09 00:29:36 +0200

    remove select border email

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,13 @@
           </div>
         </v-app-bar-title>
 
-        <input type="text" value="zannialexandre@outlook.fr" size="4" style="text-align: center; width: 300px;">
+        <input
+          id="myInput"
+          type="text"
+          value="zannialexandre@outlook.fr"
+          size="4"
+          style="text-align: center; width: 300px; border: none; outline: none;"
+        >
 
         <v-btn-alt
           prepend-icon="mdi-email"
@@ -144,98 +150,12 @@
             </v-row>
           </v-container>
         </section>
-
-        <!-- <v-container>
-          <v-divider class="my-12" />
-        </v-container> -->
-
-        <!-- <section id="controls">
-          <v-container>
-            <v-row justify="space-between">
-              <v-col cols="auto">
-                <v-responsive width="350">
-                  <h2 class="text-h4">
-                    Pour tous vos projets
-                  </h2>
-
-                  <p class="text-success mt-3">
-                    Composition API
-                  </p>
-
-                  <p class="mt-3">
-                    Vuetify 3 uses the Vue composition API to build easy-to-use and feature rich components that work out of the box.
-                  </p>
-
-                  <p class="mt-8">
-                    <strong>How to use:</strong>
-
-                    Services are now accessed through <strong>use functions</strong> that follow the Vue 3 nomenclature and code styling.
-                  </p>
-
-                  <v-btn
-                    class="mt-6"
-                    href="https://vuetifyjs.com/introduction/why-vuetify/"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    More Information
-                  </v-btn>
-                </v-responsive>
-              </v-col>
-
-              <v-img
-                max-width="400"
-                src="https://cdn.vuetifyjs.com/store/themes/vite-free/slider.png"
-              />
-            </v-row>
-          </v-container>
-        </section> -->
       </v-sheet>
 
       <v-sheet
         class="py-16"
         color="#181818"
       >
-        <!-- <section id="grid">
-          <v-container>
-            <v-row justify="space-between">
-              <v-col cols="auto">
-                <v-responsive
-                  class="overflow-visible"
-                  width="350"
-                >
-                  <h2 class="text-h4">
-                    Pour tous vos projets
-                  </h2>
-
-                  <p class="text-success mt-3">
-                    Support Development
-                  </p>
-
-                  <strong>Become a sponsor</strong>
-
-                  <p class="mt-8">
-                    Vuetify is free to use software under the <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT</a> License and requires an extensive amount of time to maintain. Supporting development ensures Vuetify will be actively maintained.
-                  </p>
-
-                  <v-btn-alt
-                    class="mt-6 "
-                    href="https://github.com/sponsors/johnleider"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    text="Support now"
-                    prepend-icon="$vuetify"
-                  />
-                </v-responsive>
-              </v-col>
-
-              <v-img
-                max-width="400"
-                src="https://cdn.vuetifyjs.com/store/themes/vite-free/vuetify.png"
-              />
-            </v-row>
-          </v-container>
-        </section> -->
       </v-sheet>
     </v-main>
 
@@ -247,7 +167,12 @@
       </v-container>
       <v-container class="text-overline d-flex align-center justify-space-between">
         <v-input>
-          <input id="myInput" type="text" value="zannialexandre@outlook.fr" size="4" style="text-align: center; width: 300px;">
+          <input
+            type="text"
+            value="zannialexandre@outlook.fr"
+            size="4"
+            style="text-align: center; width: 300px; border: none; outline: none;"
+          >
         </v-input>
         <v-btn-alt
           prepend-icon="mdi-email"
